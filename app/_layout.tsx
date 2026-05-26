3834906ed7795763f4aaa3760067f60975d2ef92
Bryan Quispe Diaz <bryan.null@proton.me>
2026-05-26 09:00:24 +0200

    feat: add design system and dark/light theme support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,39 @@
 import { Stack } from 'expo-router';
-import { PaperProvider } from 'react-native-paper';
+import { PaperProvider, MD3LightTheme, MD3DarkTheme } from 'react-native-paper';
+import { useColorScheme } from 'react-native';
+import { colors } from '../constants/theme';
 
 export default function RootLayout() {
+  const scheme = useColorScheme();
+
+  const lightTheme = {
+    ...MD3LightTheme,
+    colors: {
+      ...MD3LightTheme.colors,
+      primary: colors.light.primary,
+      secondary: colors.light.secondary,
+      background: colors.light.background,
+      surface: colors.light.surface,
+      error: colors.light.error,
+    },
+  };
+
+  const darkTheme = {
+    ...MD3DarkTheme,
+    colors: {
+      ...MD3DarkTheme.colors,
+      primary: colors.dark.primary,
+      secondary: colors.dark.secondary,
+      background: colors.dark.background,
+      surface: colors.dark.surface,
+      error: colors.dark.error,
+    },
+  };
+
+  const theme = scheme ==='dark' ? darkTheme : lightTheme
+  
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
---
+++ constants/theme.ts
@@ -0,0 +1,54 @@
+import { useColorScheme } from 'react-native';
+
+export const colors = {
+    light: {
+        primary: '#6200EE',
+        secondary: '#03DAC6',
+        background: '#F6F6F6',
+        surface: '#FFFFFF',
+        Text: '#1C1C1E',
+        textSecondary: '#6B6B6B',
+        error: '#B00020',
+        border: '#E0E0E0',
+    },
+    dark: {
+        primary: '#BB86FC',
+        secondary: '#03DAC6',
+        background: '#121212',
+        surface: '#1E1E1E',
+        Text: '#FFFFFF',
+        textSecondary: '#A0A0A0',
+        error: '#CF6679',
+        border: '#2C2C2C',
+    },
+
+};
+
+export const typography = {
+     sizes: {
+    xs: 12,
+    sm: 14,
+    md: 16,
+    lg: 20,
+    xl: 24,
+    xxl: 32,
+  },
+  weights: {
+    regular: '400' as const,
+    medium: '500' as const,
+    bold: '700' as const,
+  },
+};
+
+export const spacing = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+};
+
+export const useAppTheme = () => {
+  const scheme = useColorScheme();
+  return scheme === 'dark' ? colors.dark : colors.light;
+};
