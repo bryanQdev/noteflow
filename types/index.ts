fef1ff0f056f78d99af29ebc537e17c500e43e48
Bryan Quispe Diaz <bryan.null@proton.me>
2026-05-27 10:15:50 +0200

    feat: agregando modelo de datos TypeSript y type guards

---
+++ types/index.ts
@@ -0,0 +1,40 @@
+export interface BaseNote {
+    id: string;
+    title: string;
+    createdAt: Date;
+    updateAt: Date;
+}
+
+export interface Note extends BaseNote {
+    content: string;
+}
+
+export interface ChecklistItem {
+    id: string;
+    text: string;
+    isCompleted: boolean;
+}
+
+export interface ChecklistNote extends BaseNote {
+    items: ChecklistItem[];
+} 
+
+export interface IdeaNote extends BaseNote {
+    tags: string[];
+    color: string;
+}
+
+export type AnyNote = Note | ChecklistNote | IdeaNote;
+
+//Type guards
+export const isNote = (note: AnyNote): note is Note => {
+    return 'content' in note;
+};
+
+export const isCheckListNote = (note: AnyNote): note is ChecklistNote => {
+    return 'items' in note;
+};
+
+export const isIdeaNote = (note: AnyNote): note is IdeaNote => {
+    return 'tags' in note;
+};
